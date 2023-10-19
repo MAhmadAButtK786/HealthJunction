99cc8d027dd580ab601eb6779f5a23eae6041981
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-10-19 12:22:17 +0500

    add some images in constant's image file

--- lib/src/constants/image_string.dart
+++ lib/src/constants/image_string.dart
@@ -1,9 +1,9 @@
 const String tSplashImage =
     "assets/images/Dark Blue and Turquoise Modern Gradient Pharmacy Health Logo.png";
 const String googleLogo = "assets/images/google icon.png";
-const String bloodBank = "assets/images/blood-bank.png";
-const String clinic = "assets/images/clinic.png";
-const String hospital = "assets/images/clinic.png";
+const String bloodBank = "assets/images/bloodbankh.jpeg";
+const String clinic = "assets/images/clinich.jpeg";
+const String hospital = "assets/images/hospitalh.png";
 const String back = "assets/images/back.jpg";
-const String medicine = "assets/images/medicine.png";
-const String tProfileI = "assets/images/profileImage.jpg";
+const String medicine = "assets/images/medicineh.jpeg";
+const String tProfileI = "assets/images/women.png";
