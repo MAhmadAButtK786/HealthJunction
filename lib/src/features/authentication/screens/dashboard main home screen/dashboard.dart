9e56c56baa95288014d71ec07f02a1d784cfd77a
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-10-22 13:11:17 +0500

    Adding some more property in dashboard

--- lib/src/features/authentication/screens/dashboard main home screen/dashboard.dart
+++ lib/src/features/authentication/screens/dashboard main home screen/dashboard.dart
@@ -23,152 +23,155 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          color: Colors.indigo,
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            color: Colors.indigo,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
 
-                    /* borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
-
-                  ),*/
-                    ),
-                height: height * 0.25,
-                width: width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 35,
-                        left: 20,
-                        right: 20,
+                      /* borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+    
+                    ),*/
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          InkWell(
-                            onTap: () {},
-                            child: Icon(
-                              Icons.menu,
-                              color: Colors.white,
-                              size: 40,
-                            ),
-                          ),
-                          Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25),
+                  height: height * 0.25,
+                  width: width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 35,
+                          left: 20,
+                          right: 20,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InkWell(
+                              onTap: () {},
+                              child: Icon(
+                                Icons.menu,
                                 color: Colors.white,
-                                image: DecorationImage(
-                                    image: AssetImage(
-                                  tProfileI,
-                                ))),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 20,
-                        left: 30,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            " Health Junction",
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 1,
+                                size: 40,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            " Innovative App for Health Care",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              letterSpacing: 1,
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                height: height * 0.75,
-                width: width,
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 0.8,
-                      mainAxisSpacing: 45,
-                      crossAxisSpacing: 20),
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: imgData.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        if (index == 0) {
-                          Get.to(() => Clinichome());
-                        } else if (index == 1) {
-                          Get.to(() => Hospitalhome());
-                        } else if (index == 2) {
-                          Get.to(() => BloodBankhome());
-                        } else if (index == 3) {
-                          Get.to(() => Medicinehome());
-                        }
-                      },
-                      child: Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black26,
-                              spreadRadius: 7,
-                              blurRadius: 9,
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25),
+                                  color: Colors.white,
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                    tProfileI,
+                                  ))),
                             ),
                           ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 20,
+                          left: 30,
                         ),
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Image.asset(
-                              imgData[index],
-                              width: 100,
+                            Text(
+                              " Health Junction",
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
                             ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              " Innovative App for Health Care",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                letterSpacing: 1,
+                              ),
+                            )
                           ],
                         ),
-                      ),
-                    );
-                  },
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  height: height * 0.75,
+                  width: width,
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.8,
+                        mainAxisSpacing: 45,
+                        crossAxisSpacing: 20),
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: imgData.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          if (index == 0) {
+                            Get.to(() => Clinichome());
+                          } else if (index == 1) {
+                            Get.to(() => Hospitalhome());
+                          } else if (index == 2) {
+                            Get.to(() => BloodBankhome());
+                          } else if (index == 3) {
+                            Get.to(() => Medicinehome());
+                          }
+                        },
+                        child: Container(
+                          margin:
+                              EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                spreadRadius: 7,
+                                blurRadius: 9,
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image.asset(
+                                imgData[index],
+                                width: 100,
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
