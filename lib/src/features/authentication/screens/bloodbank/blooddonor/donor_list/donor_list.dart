955482776acf3c07c69dbd249aedb855994fb7c7
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-10-28 20:59:59 +0500

    add firebase in donor lisrt and recipient list

--- lib/src/features/authentication/screens/bloodbank/blooddonor/donor_list/donor_list.dart
+++ lib/src/features/authentication/screens/bloodbank/blooddonor/donor_list/donor_list.dart
@@ -1,42 +1,126 @@
-// ignore_for_file: unnecessary_import, prefer_const_constructors, unused_import, override_on_non_overriding_member
+// ignore_for_file: prefer_const_constructors, avoid_print, unused_element, unused_import, use_key_in_widget_constructors, unused_local_variable, deprecated_member_use, sort_child_properties_last, prefer_const_literals_to_create_immutables, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:healthjunction/src/constants/colors.dart';
-import 'package:healthjunction/src/constants/text_string.dart';
+import 'package:healthjunction/src/features/authentication/screens/clinicmodulescreens/clinicscreen2.dart';
 import 'package:healthjunction/src/features/authentication/screens/navbar/navbar.dart';
-import 'package:healthjunction/src/features/authentication/screens/profile_screen/profile_screen.dart';
 import 'package:healthjunction/src/features/authentication/screens/sidebar/sidebar.dart';
-import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class DonorList extends StatefulWidget {
-  const DonorList({Key? key}) : super(key: key);
-
-  @override
-  State<DonorList> createState() => _DonorListState();
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+void _handleMenuPressed() {
+  scaffoldKey.currentState?.openDrawer();
 }
 
-class _DonorListState extends State<DonorList> {
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-  @override
-  void _handleMenuPressed() {
-    scaffoldKey.currentState?.openDrawer();
-  }
-
+class DonorList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       appBar: Navbar(
           color: Colors.red,
-          textNav: "Donor Lists",
+          textNav: "Donor List",
           onMenuPressed: _handleMenuPressed),
-      drawer: ReusableDrawerSideBar(
-        color: Colors.red, // Set your desired color
-        headerText: 'Donor Lists',
-      ), // rest of your code
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Donor Registration'),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+            // Add more items here
+          ],
+        ),
+      ),
+      body: FutureBuilder<List<DocumentSnapshot>>(
+        future: fetchDonorData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData && snapshot.data!.isEmpty) {
+            return Center(child: Text('No donor data available.'));
+          } else if (snapshot.hasData) {
+            List<DocumentSnapshot> donors = snapshot.data!;
+            SizedBox(
+              height: 20,
+            );
+            return ListView.separated(
+              itemCount: donors.length,
+              separatorBuilder: (BuildContext context, int index) => Divider(),
+              itemBuilder: (context, index) {
+                var donor = donors[index].data() as Map<String, dynamic>;
+                String fullName = donor['FullName'] ?? 'No Name';
+                String email = donor['Email'] ?? 'No Email';
+                String bloodType = donor['BloodType'] ?? 'No Blood Type';
+                String phoneNumber = donor['Phone'] ?? 'No Phone Number';
+                String city = donor['City'] ?? 'No City Added';
+                String province = donor['Province'] ?? 'No Province Added';
+                return ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    fullName,
+                    style: GoogleFonts.montserrat(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                  ),
+                  subtitle: Text(
+                    'Email: $email\nBlood Type: $bloodType\nPhone Number: $phoneNumber\nCity: $city\nProvince:$province',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.phone,
+                    color: Colors.red,
+                  ),
+                  onTap: () async {
+                    String phone = phoneNumber;
+                    String url = 'tel:$phone';
+                    try {
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    } catch (e) {
+                      print('Error: $e');
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('Error: Could not make a phone call'),
+                      ));
+                    }
+                  },
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('Unknown error occurred.'));
+          }
+        },
+      ),
     );
+  }
+
+  Future<List<DocumentSnapshot>> fetchDonorData() async {
+    List<DocumentSnapshot> donors = [];
+    try {
+      QuerySnapshot<Map<String, dynamic>> donorData =
+          await FirebaseFirestore.instance.collection('Donors').get();
+      donors = donorData.docs;
+    } catch (error) {
+      // Handle errors here
+      print('Error fetching donor data: $error');
+    }
+    return donors;
   }
 }
