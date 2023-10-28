4678ba295e8852f05888202ef9dab3030d3ecfd2
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-10-28 16:27:42 +0500

    Remove Warning

--- lib/src/features/authentication/repository/recipient_repository.dart
+++ lib/src/features/authentication/repository/recipient_repository.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, body_might_complete_normally_catch_error
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
