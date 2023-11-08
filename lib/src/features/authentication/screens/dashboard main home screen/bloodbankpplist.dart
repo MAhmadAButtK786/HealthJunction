eebf3ac3e2513c0be56fec6141059cd3ca83e8a2
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-11-08 20:24:54 +0500

    Remove Some Warnings

--- lib/src/features/authentication/screens/dashboard main home screen/bloodbankpplist.dart
+++ lib/src/features/authentication/screens/dashboard main home screen/bloodbankpplist.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_typing_uninitialized_variables, unnecessary_import, unused_import, must_be_immutable
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
--- lib/src/features/authentication/screens/profile_icon_functions/profile_page/profile_main_page.dart
+++ lib/src/features/authentication/screens/profile_icon_functions/profile_page/profile_main_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables, unused_import
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables, unused_import, no_leading_underscores_for_local_identifiers
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
