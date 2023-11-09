bb07b8df261e067c9226cf6709dd289cecd104d1
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-11-09 11:50:32 +0500

    Start Working on Private and Public BB Screens

--- lib/src/features/authentication/screens/bloodbank/BloodBanks List/publicbb.dart
+++ lib/src/features/authentication/screens/bloodbank/BloodBanks List/publicbb.dart
@@ -1,7 +1,10 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, prefer_const_constructors
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:healthjunction/src/features/authentication/screens/bloodbank/BloodBanks%20List/privatebb.dart';
+import 'package:healthjunction/src/features/authentication/screens/navbar/navbar.dart';
+import 'package:healthjunction/src/features/authentication/screens/sidebar/sidebar.dart';
 
 class PublicBB extends StatefulWidget {
   const PublicBB({super.key});
@@ -10,9 +13,24 @@ class PublicBB extends StatefulWidget {
   State<PublicBB> createState() => _PublicBBState();
 }
 
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+void _handleMenuPressed() {
+  scaffoldKey.currentState?.openDrawer();
+}
+
 class _PublicBBState extends State<PublicBB> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+            drawer: ReusableDrawerSideBar(
+              color: Colors.red,
+              headerText: "Blood Banks",
+            ),
+            key: scaffoldKey,
+            appBar: Navbar(
+                color: Colors.red,
+                textNav: "Blood Banks",
+                onMenuPressed: _handleMenuPressed)));
   }
 }
