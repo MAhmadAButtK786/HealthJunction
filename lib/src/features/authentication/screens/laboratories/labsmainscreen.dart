139d3ecbf999160b3bcd062bc44742230deb0edd
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-11-09 17:25:50 +0500

    Complete Working on the Labs Main

--- lib/src/features/authentication/screens/laboratories/labsmainscreen.dart
+++ lib/src/features/authentication/screens/laboratories/labsmainscreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, must_be_immutable, unused_import
+// ignore_for_file: prefer_const_constructors, avoid_print, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:healthjunction/src/constants/colors.dart';
@@ -9,16 +9,26 @@ import 'package:healthjunction/src/features/authentication/screens/sidebar/sideb
 class LabsMainScreen extends StatelessWidget {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   LabsMainScreen({super.key});
-  List title = [
+  List titleN = [
     "Allied Lahore Lab",
     "Chughtai Lab",
     "Indus Lab",
     "Health Zone Lab",
     "Alnoor Diagnostic Center",
     "Islamabad Diagnostic Center",
-    "Lahore Medical Lab and Diagnostic Center"
+    "Lahore Medical Lab and Diagnostic Center",
+    "Registered Labs In Pakistan By Government",
   ];
-  List image = [alliedL, chugtai, induslab, healthZone, alnoor, idc, lmldc];
+  List image = [
+    alliedL,
+    chugtai,
+    induslab,
+    healthZone,
+    alnoor,
+    idc,
+    lmldc,
+    govt
+  ];
   void _handleMenuPressed() {
     scaffoldKey.currentState?.openDrawer();
   }
@@ -27,19 +37,60 @@ class LabsMainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            backgroundColor: clab,
-            key: scaffoldKey,
-            drawer: ReusableDrawerSideBar(
-              color: clab,
-              headerText: "Laboratories",
+      backgroundColor: clab,
+      key: scaffoldKey,
+      drawer: ReusableDrawerSideBar(
+        color: clab,
+        headerText: "Laboratories",
+      ),
+      appBar: Navbar(
+          color: clab,
+          textNav: "Laboratories",
+          onMenuPressed: _handleMenuPressed),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 10, top: 10),
+            child: Image(image: AssetImage(labms)),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: titleN.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.all(8.0), // Add space around the Container
+                  child: Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage(image[index]),
+                        radius: 20,
+                      ),
+                      title: Text(
+                        titleN[index],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                      subtitle: Text(
+                        'Click for more details',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      onTap: () {
+                        print("Tapped on ${titleN[index]}");
+                      },
+                    ),
+                  ),
+                );
+              },
             ),
-            appBar: Navbar(
-                color: clab,
-                textNav: "Laboratories",
-                onMenuPressed: _handleMenuPressed),
-            body: Container(
-              padding: EdgeInsets.only(),
-              child: Image(image: AssetImage(labms)),
-            )));
+          ),
+        ],
+      ),
+    ));
   }
 }
