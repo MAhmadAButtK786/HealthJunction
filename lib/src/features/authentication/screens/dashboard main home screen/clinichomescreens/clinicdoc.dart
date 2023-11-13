38a5d21346aaf214dbfc1987453c97fa46e732d9
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-11-13 23:08:49 +0500

    Create a clinic registrationpage

--- lib/src/features/authentication/screens/dashboard main home screen/clinichomescreens/clinicdoc.dart
+++ lib/src/features/authentication/screens/dashboard main home screen/clinichomescreens/clinicdoc.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, must_be_immutable, prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_import
+// ignore_for_file: prefer_typing_uninitialized_variables, must_be_immutable, prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_import, unused_import
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -205,9 +205,6 @@ class ClinicRegistraionmain extends StatelessWidget {
         Get.to(() => clinicpage1());
         break;
       case 1:
-        Get.to(() => clinicpage3());
-        break;
-      case 2:
         Get.to(() => ClinicRegistration());
         break;
     }
