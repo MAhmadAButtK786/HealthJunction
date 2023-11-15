b0d75e11e109bc1cf2167b813f2be64cdc51fc58
MAhmadAButtK786 <arifmuhammadahmad@gmail.com>
2023-11-15 15:51:16 +0500

    Add some Changing in the UI of Footer of Login Screen

--- lib/src/features/authentication/screens/loginscreen/login_footer_widget.dart
+++ lib/src/features/authentication/screens/loginscreen/login_footer_widget.dart
@@ -127,62 +127,62 @@ class LoginFooterWidget extends StatelessWidget {
     }
   }
 
-  // Twitter(X) Authentication
-  Future<UserCredential?> _signInWithTwitter() async {
-    try {
-      // Create a TwitterLogin instance
-      final twitterLogin = TwitterLogin(
-        apiKey: '0PGJRuluVJPiCJbbpTQsbsVvQ',
-        apiSecretKey: 'hrSawbTfy6kndShUMG89DLdk7DqYA4McmEOlZcFlWOH5pWm7Fx',
-        redirectURI:
-            'https://health-junction-675ff.firebaseapp.com/__/auth/handler',
-      );
+  // // Twitter(X) Authentication
+  // Future<UserCredential?> _signInWithTwitter() async {
+  //   try {
+  //     // Create a TwitterLogin instance
+  //     final twitterLogin = TwitterLogin(
+  //       apiKey: '0PGJRuluVJPiCJbbpTQsbsVvQ',
+  //       apiSecretKey: 'hrSawbTfy6kndShUMG89DLdk7DqYA4McmEOlZcFlWOH5pWm7Fx',
+  //       redirectURI:
+  //           'https://health-junction-675ff.firebaseapp.com/__/auth/handler',
+  //     );
 
-      // Trigger the sign-in flow
-      final authResult = await twitterLogin.login();
-      if (authResult == null) {
-        print('User cancelled the sign-in process');
-        return null;
-      }
+  //     // Trigger the sign-in flow
+  //     final authResult = await twitterLogin.login();
+  //     if (authResult == null) {
+  //       print('User cancelled the sign-in process');
+  //       return null;
+  //     }
 
-      // Create a Twitter auth credential from the access token
-      final OAuthCredential twitterAuthCredential =
-          TwitterAuthProvider.credential(
-        accessToken: authResult.authToken!,
-        secret: authResult.authTokenSecret!,
-      );
+  //     // Create a Twitter auth credential from the access token
+  //     final OAuthCredential twitterAuthCredential =
+  //         TwitterAuthProvider.credential(
+  //       accessToken: authResult.authToken!,
+  //       secret: authResult.authTokenSecret!,
+  //     );
 
-      // Sign in to Firebase with the Twitter auth credential
-      UserCredential userCredential = await FirebaseAuth.instance
-          .signInWithCredential(twitterAuthCredential);
+  //     // Sign in to Firebase with the Twitter auth credential
+  //     UserCredential userCredential = await FirebaseAuth.instance
+  //         .signInWithCredential(twitterAuthCredential);
 
-      // User is signed in. You can handle the signed-in user here.
-      print("User signed in: ${userCredential.user!.displayName}");
+  //     // User is signed in. You can handle the signed-in user here.
+  //     print("User signed in: ${userCredential.user!.displayName}");
 
-      // Update user details in Firestore
-      String? email = userCredential.user!.email;
-      String id = userCredential.user!.uid;
-      String? fullName = userCredential.user!.displayName;
-      String? password = '';
-      String? phoneNumber = '';
+  //     // Update user details in Firestore
+  //     String? email = userCredential.user!.email;
+  //     String id = userCredential.user!.uid;
+  //     String? fullName = userCredential.user!.displayName;
+  //     String? password = '';
+  //     String? phoneNumber = '';
 
-      // Store user data in Firestore
-      await FirebaseFirestore.instance.collection('Users').doc(id).set({
-        'id': id,
-        'fullName': fullName,
-        'email': email,
-        'phoneNumber': phoneNumber,
-        'password': password,
-      });
+  //     // Store user data in Firestore
+  //     await FirebaseFirestore.instance.collection('Users').doc(id).set({
+  //       'id': id,
+  //       'fullName': fullName,
+  //       'email': email,
+  //       'phoneNumber': phoneNumber,
+  //       'password': password,
+  //     });
 
-      // Once signed in and profile is updated, return the UserCredential
-      return userCredential;
-    } catch (e) {
-      // Handle authentication errors
-      print("Error Signing In with Twitter: $e");
-      return null;
-    }
-  }
+  //     // Once signed in and profile is updated, return the UserCredential
+  //     return userCredential;
+  //   } catch (e) {
+  //     // Handle authentication errors
+  //     print("Error Signing In with Twitter: $e");
+  //     return null;
+  //   }
+  // }
 
   Widget build(BuildContext context) {
     return Align(
@@ -207,7 +207,7 @@ class LoginFooterWidget extends StatelessWidget {
           ),
           SizedBox(
             height: 50,
-            width: 150,
+            width: 100,
             child: Align(
               alignment: Alignment.center,
               child: Row(
@@ -250,21 +250,21 @@ class LoginFooterWidget extends StatelessWidget {
                   ),
                   //X(Twitter) Button
 
-                  IconButton(
-                    icon: Image(
-                      image: AssetImage(twitterX),
-                      width: 30.0,
-                    ),
-                    onPressed: () async {
-                      UserCredential? userCredential =
-                          await _signInWithTwitter();
-                      if (userCredential != null) {
-                        // User signed in successfully, you can handle the user data here
-                        print('User signed in with X: ${userCredential.user}');
-                        Get.to(() => Dashboard()); // Navigate to Dashboard
-                      }
-                    },
-                  ),
+                  // IconButton(
+                  //   icon: Image(
+                  //     image: AssetImage(twitterX),
+                  //     width: 30.0,
+                  //   ),
+                  //   onPressed: () async {
+                  //     UserCredential? userCredential =
+                  //         await _signInWithTwitter();
+                  //     if (userCredential != null) {
+                  //       // User signed in successfully, you can handle the user data here
+                  //       print('User signed in with X: ${userCredential.user}');
+                  //       Get.to(() => Dashboard()); // Navigate to Dashboard
+                  //     }
+                  //   },
+                  // ),
                 ],
               ),
             ),
