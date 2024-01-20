295e0c459035f2e878602e94ef5fc1852d6880ab
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-01-20 13:38:58 +0500

    Complete the BloodBank Home Page

---
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/bloodbanks/bbHome/bbhdata.js
@@ -0,0 +1,40 @@
+const bloodBankData = [
+    {
+      title: 'Donate Blood',
+      description: 'Join our life-saving mission. Your blood donation can make a huge difference and save lives.',
+      link: '/donate',
+      icon: '💉', // Syringe icon for blood donation
+    },
+    {
+      title: 'Find a Blood Drive',
+      description: 'Participate in our blood drives. Your contribution can help us reach more people in need.',
+      link: '/blood-drives',
+      icon: '📍', // Pin icon for location
+    },
+    {
+      title: 'Become a Donor',
+      description: 'Become a hero. Learn how you can join our community of life-savers.',
+      link: '/become-donor',
+      icon: '🦸', // Superhero icon for donors
+    },
+    {
+      title: 'Find Recipients',
+      description: 'Connect with those in need. Your donation can bring hope to someone\'s life.',
+      link: '/find-recipients',
+      icon: '🤝', // Handshake icon for connection
+    },
+    {
+      title: 'Blood Banks Directory',
+      description: 'Discover our network of blood banks. We ensure safe and reliable blood supply.',
+      link: '/blood-banks',
+      icon: '🌐', // Globe icon for network
+    },
+    {
+      title: 'Contact Us',
+      description: 'We\'re here to help. Reach out to us for any queries or assistance.',
+      link: '/contact',
+      icon: '📨', // Mail icon for contact
+    },
+  ];
+  
+  export default bloodBankData;
--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/bloodbanks/bloodbank.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/bloodbanks/bloodbank.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import BBNavbar from './BBNavbar'
+import BloodBankHomePage from './bbHome/bloodhome'
 import Footer from '../footer'
 export default function bloodbank() {
   return (
     <div>
       <BBNavbar/>
-      
+      <BloodBankHomePage/>
       <Footer/>
     </div>
   )
