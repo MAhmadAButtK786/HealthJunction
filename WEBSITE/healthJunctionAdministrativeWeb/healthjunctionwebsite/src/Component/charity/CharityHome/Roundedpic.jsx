e4b1a52e40e5858002f9202ae7577ef3e9a9e5e5
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-01-28 19:31:50 +0500

    Add Logos of Org

--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/charity/CharityHome/Roundedpic.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/charity/CharityHome/Roundedpic.jsx
@@ -5,38 +5,38 @@ function Roundedpic() {
   return (
     <>
       <div className="container">
-        <p className="text-2xl text-purple-900 font-bold p-4 md:p-8 text-center">
+        <p className="p-4 text-2xl font-bold text-center text-purple-900 md:p-8">
           "Consider supporting an organization that aligns with your values and
           has a positive impact, deserving of your charitable contributions."
         </p>
       </div>
-      <div className="flex flex-wrap justify-center gap-6 sm:gap-8 md:gap-10 lg:gap-12 py-4 sm:py-6 md:py-10 px-4 sm:px-6 md:px-10 lg:px-12">
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+      <div className="flex flex-wrap justify-center gap-6 px-4 py-4 sm:gap-8 md:gap-10 lg:gap-12 sm:py-6 md:py-10 sm:px-6 md:px-10 lg:px-12">
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Logos/Aga Khan Foundation.png" alt="Aga Khan Foundation" className="object-cover w-full h-full rounded-full" />
         </div>
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Logos/Akhuwat Foundation.jpg" alt="Akhuwat Foundation" className="object-cover w-full h-full rounded-full" />
         </div>
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Logos/Al-Khidmat Foundation Pakistan.png" alt="Al-Khidmat Foundation Pakistan" className="object-cover w-full h-full rounded-full" />
         </div>
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Logos/Aurat Foundation.png" alt="Aurat Foundation" className="object-cover w-full h-full rounded-full" />
         </div>
         {/* Repeat the above div structure for other images */}
       </div>
-      <div className="flex flex-wrap justify-center gap-6 sm:gap-8 md:gap-10 lg:gap-12 py-4 sm:py-6 md:py-10 px-4 sm:px-6 md:px-10 lg:px-12">
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+      <div className="flex flex-wrap justify-center gap-6 px-4 py-4 sm:gap-8 md:gap-10 lg:gap-12 sm:py-6 md:py-10 sm:px-6 md:px-10 lg:px-12">
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Logos/ansar.png" alt="" className="object-cover w-full h-full rounded-full" />
         </div>
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Logos/chippa.png" alt="" className="object-cover w-full h-full rounded-full" />
         </div>
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Blood.png" alt="" className="object-cover w-full h-full rounded-full" />
         </div>
-        <div className="flex-1 sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <img src="images/Blood.png" alt="" className="rounded-full object-cover w-full h-full" />
+        <div className="flex-1 transition-transform duration-300 ease-in-out transform sm:w-1/2 md:w-1/3 lg:w-1/4 max-h-md hover:scale-105">
+          <img src="images/Blood.png" alt="" className="object-cover w-full h-full rounded-full" />
         </div>
         {/* Repeat the above div structure for other images */}
       </div>
