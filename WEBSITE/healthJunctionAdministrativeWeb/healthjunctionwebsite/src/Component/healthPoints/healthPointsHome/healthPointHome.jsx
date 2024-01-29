d774e4ab3f1889b91b96f00a88801fcd3e521a29
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-01-29 21:07:59 +0500

    Health Points Home Screen created

--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/healthPoints/healthPointsHome/healthPointHome.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/healthPoints/healthPointsHome/healthPointHome.jsx
@@ -3,12 +3,14 @@ import HPNavbar from '../hpNavbar';
 import Footer from '../../footer';
 import HPHomecards from './healthhadith';
 import HPHomeCarousel from './carouselHP';
+import CardsHP from './cardsHP';
 function HealthPointHome() {
   return (
     <>
     <HPNavbar/>
     <HPHomecards/>
     <HPHomeCarousel/>
+    <CardsHP/>
     <Footer/>
     </>
   )
