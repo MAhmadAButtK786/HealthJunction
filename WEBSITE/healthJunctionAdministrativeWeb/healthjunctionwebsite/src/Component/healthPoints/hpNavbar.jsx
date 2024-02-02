ec023907d25ac1083c9f97ee85858c97cbcefef9
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-02-03 00:21:40 +0500

    change in hpnavbar

--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/healthPoints/hpNavbar.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/healthPoints/hpNavbar.jsx
@@ -85,7 +85,7 @@ export default function HPNavbar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Home
@@ -95,7 +95,7 @@ export default function HPNavbar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/contact"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             About Us
