d360fc7549960fd634736eff9886c7568a6fa6e5
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-02-04 01:09:48 +0500

    Change in The UI of the Screens

--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/charity/CharityHome/homecharity.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/charity/CharityHome/homecharity.jsx
@@ -3,11 +3,13 @@ import CharityNavbar from '../charityNavbar'
 import Footer from '../../footer';
 import CHomecards from './homecards';
 import Roundedpic from './Roundedpic';
+import CStory from './story';
 function Homecharity() {
   return (
     <div>
       <CharityNavbar/>
       <CHomecards/>
+      <CStory />
       <Roundedpic/>
       <Footer/>
     </div>
--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/charity/our Partner/partner.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/charity/our Partner/partner.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import CharityNavbar from '../charityNavbar'
 import Footer from '../../footer'
 import PartnerDetails from './partnerDetails'
+
 function Partner() {
   return (
 <>
 <CharityNavbar/>
 <PartnerDetails/>
+
 <Footer></Footer>
 </>
   )
