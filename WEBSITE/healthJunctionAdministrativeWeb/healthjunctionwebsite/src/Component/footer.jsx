a378732540017db2e451aafe6ae66813ee58aa4a
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-02-04 01:51:39 +0500

    Change in the UI

--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/footer.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <footer className="bg-blue-100/80 font-sans dark:bg-gray-900">
-        <div className="container px-4 sm:px-6 lg:px-8 py-4 mx-auto">
+      <footer className="font-sans bg-blue-100/80 dark:bg-gray-900">
+        <div className="container px-4 py-4 mx-auto sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-y-4 md:grid-cols-3 lg:grid-cols-4">
             <div className="sm:col-span-2 md:col-span-1 lg:col-span-2">
               <h1 className="text-lg font-semibold tracking-tight text-gray-800 dark:text-white">
                 Our Vision
               </h1>
-              <div className="mt-2 space-y-4 text-gray-600 dark:text-gray-300 col-span-1">
+              <div className="col-span-1 mt-2 space-y-4 text-gray-600 dark:text-gray-300">
                 <p className="leading-relaxed">
                   We envision a world where healthcare is a fundamental right
                   accessible to everyone. Our mission is to ensure that every
@@ -27,15 +27,21 @@ const Footer = () => {
                 Quick Links
               </p>
               <div className="flex flex-col items-start mt-2 space-y-2">
+                <Link to= "/home">
                 <p className="text-gray-600 hover:underline hover:cursor-pointer hover:text-blue-500">
                   Home
                 </p>
+                </Link>
+                <Link to="/contact">
                 <p className="text-gray-600 hover:underline hover:cursor-pointer hover:text-blue-500">
                   Who We Are
                 </p>
+                </Link>
+                <Link to="/contact">
                 <p className="text-gray-600 hover:underline hover:cursor-pointer hover:text-blue-500">
                   Our Philosophy
                 </p>
+                </Link>
               </div>
             </div>
             <div className="md:col-span-1 lg:col-span-1">
@@ -43,29 +49,35 @@ const Footer = () => {
                 Services
               </p>
               <div className="flex flex-col items-start mt-2 space-y-2">
-                <a
-                  href="https://example.com/hospitals"
+                <Link
+                  to="/healthPointHome"
                   className="text-gray-600 hover:underline hover:cursor-pointer hover:text-blue-500"
                 >
                   Hospitals & Clinics
-                </a>
-                <a
-                  href="https://example.com/blood-banks"
+                </Link>
+                <Link
+                to="/bloodbank"
                   className="text-gray-600 hover:underline hover:cursor-pointer hover:text-blue-500"
                 >
                   Blood Banks
-                </a>
-                <a
-                  href="https://example.com/charity-lab-info"
+                </Link>
+                <Link
+                to="/homecharity"
                   className="text-gray-600 hover:underline hover:cursor-pointer hover:text-blue-500"
                 >
-                  Charity & Lab Information
-                </a>
+                  Charity 
+                </Link>
+                <Link
+                to="/Labhome"
+                  className="text-gray-600 hover:underline hover:cursor-pointer hover:text-blue-500"
+                >
+                   Lab Information
+                </Link>
               </div>
             </div>
           </div>
-          <hr className="my-4 border-gray-200 dark:border-gray-700 h-1" />
-          <div className="flex justify-between items-center flex-col md:flex-row">
+          <hr className="h-1 my-4 border-gray-200 dark:border-gray-700" />
+          <div className="flex flex-col items-center justify-between md:flex-row">
             <div className="flex gap-2">
               <a href="https://www.google.com">
                 <img
@@ -136,7 +148,7 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <p className="font-sans mt-4 text-center text-sm text-gray-600 dark:text-white">
+          <p className="mt-4 font-sans text-sm text-center text-gray-600 dark:text-white">
             © 2024 Your Company Inc. All rights reserved.
           </p>
         </div>
