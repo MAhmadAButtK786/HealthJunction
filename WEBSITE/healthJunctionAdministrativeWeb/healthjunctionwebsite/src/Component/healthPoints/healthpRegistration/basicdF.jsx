c00b2185aab18cb0d8d4a391614ed4f7afb90af1
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-02-04 14:32:49 +0500

    DEfine Folder Structure For Navbars

---
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/healthPoints/healthpRegistration/basicdF.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HPNavbar from '../../Navbars/hpNavbar';
+import Footer from '../../footer';
+function BasicdF() {
+  return (
+   <>
+   <HPNavbar/>
+   <Footer/>
+   </>
+  )
+}
+
+export default BasicdF
