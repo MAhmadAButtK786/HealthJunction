310fbb38c909045e757ff02e7cc271c76f3c8635
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-02-12 00:23:40 +0500

    add files in the system

---
+++ WEBSITE/OurPartner/BDS/bdspartner/src/tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+const { Component } = require('react');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+  ],
+
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
