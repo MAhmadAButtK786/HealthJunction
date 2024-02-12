787a24db43c82cc59d96eeb9deed9963994c0ee3
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-02-12 17:32:35 +0500

    Add Home Introduction

--- WEBSITE/OurPartner/BDS/bdspartner/src/Component/Home/bdshome.jsx
+++ WEBSITE/OurPartner/BDS/bdspartner/src/Component/Home/bdshome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
+import BDSDetails from './Introduction'
 function BDSHome() {
   return (
     <>
     <Navbar/>
+    <BDSDetails/>
     <Footer/>
     </>
   )
