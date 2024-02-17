1ec1a3c5b08195d35cec29c18259117509fc5338
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-02-17 09:20:44 +0500

    Add excelSheet created

--- WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/healthPoints/healthpRegistration/ExcelSheetPart/excelsheet.jsx
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/healthPoints/healthpRegistration/ExcelSheetPart/excelsheet.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HPNavbar from '../../hpNavbar'
 import Footer from '../../../footer'
+import ExcelUploadForm from './excelsheetsdata'
 function Excelsheet() {
   return (
     <div>
       <HPNavbar/>
+      <ExcelUploadForm/>
       <Footer/>
     </div>
   )
