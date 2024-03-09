e9ce95d8d9ff32a89975457c34d6e0a0bab86b3e
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-03-09 18:20:57 +0500

    Add HealthZone in the User Website

---
+++ WEBSITE/healthJunctionAdministrativeWeb/healthjunctionwebsite/src/Component/lab/privateLabs/LabsScreens/HealthZone/healthzone.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LabNavbar from '../../../labNavbar';
+import HealthZoneData from './HealthZoneLabData';
+import Footer from '../../../../footer';
+function HealthZone() {
+  return (
+    <div>
+      <LabNavbar/>
+      <HealthZoneData/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default HealthZone;
