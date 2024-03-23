96b7c3fc899e76b22477f2d15cad872089ae77ba
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-03-23 10:59:51 +0500

    DietDataPage

--- WEBSITE/AdminHealthJunction/adminhealthjunction/src/components/nutrifit/diets/dietsdata.jsx
+++ WEBSITE/AdminHealthJunction/adminhealthjunction/src/components/nutrifit/diets/dietsdata.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { collection, getDocs, doc, deleteDoc, query, where } from "firebase/firestore"; // Replace with database from firebase.js
-import { database } from "../../../firebase"; // Import database from firebase.js
+import {
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  query,
+  where,
+} from "firebase/firestore";
+import { database } from "../../../firebase";
+import { FaEdit, FaTrash, FaAngleDown, FaAngleUp } from "react-icons/fa";
+import "./DietDetailsPage.css";
 
 const DietDetailsPage = () => {
   const [diets, setDiets] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [expandedDietIds, setExpandedDietIds] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
@@ -29,9 +39,7 @@ const DietDetailsPage = () => {
   const handleDelete = async (dietId) => {
     try {
       await deleteDoc(doc(database, "diets", dietId));
-      setDiets((prevDiets) =>
-        prevDiets.filter((diet) => diet.id !== dietId)
-      );
+      setDiets((prevDiets) => prevDiets.filter((diet) => diet.id !== dietId));
       alert("Diet plan deleted successfully");
     } catch (error) {
       console.error("Error deleting diet plan:", error);
@@ -45,7 +53,10 @@ const DietDetailsPage = () => {
 
   const handleSearch = async () => {
     try {
-      const q = query(collection(database, "diets"), where("dietName", "==", searchTerm));
+      const q = query(
+        collection(database, "diets"),
+        where("dietName", "==", searchTerm)
+      );
       const querySnapshot = await getDocs(q);
       const searchedDiets = querySnapshot.docs.map((doc) => ({
         id: doc.id,
@@ -76,58 +87,108 @@ const DietDetailsPage = () => {
     fetchDiets();
   };
 
+  const getRandomColor = () => {
+    const colors = [
+      "#FFCDD2",
+      "#F8BBD0",
+      "#E1BEE7",
+      "#D1C4E9",
+      "#BBDEFB",
+      "#B3E5FC",
+      "#B2EBF2",
+      "#C8E6C9",
+      "#DCEDC8",
+      "#FFF9C4",
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
+  const toggleExpand = (dietId) => {
+    setExpandedDietIds((prevExpandedDietIds) => {
+      if (prevExpandedDietIds.includes(dietId)) {
+        // If the clicked diet is already expanded, remove it from the array
+        return prevExpandedDietIds.filter((id) => id !== dietId);
+      } else {
+        // Otherwise, add the clicked diet to the array
+        return [...prevExpandedDietIds, dietId];
+      }
+    });
+  };
+
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Diet Plans</h1>
-      <div className="flex items-center mb-6">
+      <h1 className="header">Diet Plans</h1>
+      <div className="search-bar">
         <input
           type="text"
           placeholder="Search Diet"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="border border-gray-300 px-4 py-2 rounded-md mr-4"
+          className="search-input"
         />
-        <button onClick={handleSearch} className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">Search</button>
-        <button onClick={resetSearch} className="bg-gray-300 px-4 py-2 rounded-md">Reset</button>
+        <button onClick={handleSearch} className="search-button">
+          Search
+        </button>
+        <button onClick={resetSearch} className="reset-button">
+          Reset
+        </button>
       </div>
-      <button onClick={() => history.push("/diet")} className="bg-green-500 text-white px-4 py-2 rounded-md mb-6">Insert Diet</button>
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th className="py-2">Diet Name</th>
-            <th className="py-2">Description</th>
-            <th className="py-2">Advantages</th>
-            <th className="py-2">Disadvantages</th>
-            <th className="py-2">Recommended Duration</th>
-            <th className="py-2">Meal Frequency</th>
-            <th className="py-2">Foods to Eat</th>
-            <th className="py-2">Foods to Avoid</th>
-            <th className="py-2">Image</th> 
-            <th className="py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {diets.map((diet) => (
-            <tr key={diet.id}>
-              <td className="py-4">{diet.dietName}</td>
-              <td className="py-4">{diet.description}</td>
-              <td className="py-4">{diet.advantages}</td>
-              <td className="py-4">{diet.disadvantages}</td>
-              <td className="py-4">{diet.recommendedDuration}</td>
-              <td className="py-4">{diet.mealFrequency}</td>
-              <td className="py-4">{diet.foodsToEat}</td>
-              <td className="py-4">{diet.foodsToAvoid}</td>
-              <td className="py-4">
-                {diet.imageUrl && <img src={diet.imageUrl} alt={diet.dietName} className="w-16 h-16 rounded-full" />}
-              </td>
-              <td className="py-4">
-                <button onClick={() => handleUpdate(diet.id)} className="bg-blue-500 text-white px-3 py-1 rounded-md mr-2">Update</button>
-                <button onClick={() => handleDelete(diet.id)} className="bg-red-500 text-white px-3 py-1 rounded-md">Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <button onClick={() => history.push("/diet")} className="insert-button">
+        Insert Diet
+      </button>
+      <div className="diet-grid">
+        {diets.map((diet) => (
+          <div key={diet.id} className="diet-card">
+            <div
+              className={`diet-header bg-${getRandomColor()}`}
+              onClick={() => toggleExpand(diet.id)}
+            >
+              <div className="diet-title">
+                <h3 className="diet-name">{diet.dietName}</h3>
+                <p className="expand-icon">
+                  {expandedDietIds.includes(diet.id) ? (
+                    <FaAngleUp />
+                  ) : (
+                    <FaAngleDown />
+                  )}
+                </p>
+              </div>
+              {expandedDietIds.includes(diet.id) && (
+                <div className="diet-details">
+                  <p>Description: {diet.description}</p>
+                  <p>Advantages: {diet.advantages}</p>
+                  <p>Disadvantages: {diet.disadvantages}</p>
+                  <p>Recommended Duration: {diet.recommendedDuration}</p>
+                  <p>Meal Frequency: {diet.mealFrequency}</p>
+                  <p>Foods to Eat: {diet.foodsToEat}</p>
+                  <p>Foods to Avoid: {diet.foodsToAvoid}</p>
+                  {diet.imageUrl && (
+                    <img
+                      src={diet.imageUrl}
+                      alt={diet.dietName}
+                      className="diet-image"
+                    />
+                  )}
+                </div>
+              )}
+            </div>
+            <div className="diet-actions">
+              <button
+                onClick={() => handleUpdate(diet.id)}
+                className="edit-button"
+              >
+                <FaEdit />
+              </button>
+              <button
+                onClick={() => handleDelete(diet.id)}
+                className="delete-button"
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
