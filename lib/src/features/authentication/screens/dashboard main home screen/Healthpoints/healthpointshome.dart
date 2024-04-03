3b631c96e79b4b90e026afa2dae690837e5313f0
Muhammad Ahmad <arifmuhammadahmad@gmail.com>
2024-04-03 12:32:37 +0500

    Remove  Unwanted Files

--- lib/src/features/authentication/screens/dashboard main home screen/Healthpoints/healthpointshome.dart
+++ lib/src/features/authentication/screens/dashboard main home screen/Healthpoints/healthpointshome.dart
@@ -7,8 +7,6 @@ import 'package:healthjunction/src/constants/image_string.dart';
 import 'package:healthjunction/src/features/authentication/screens/clinicmodulescreens/clinicscreeen3.dart';
 import 'package:healthjunction/src/features/authentication/screens/clinicmodulescreens/clinicscreen1.dart';
 import 'package:healthjunction/src/features/authentication/screens/clinicmodulescreens/docregistration.dart';
-import 'package:healthjunction/src/features/authentication/screens/dashboard%20main%20home%20screen/clinichomescreens/clinicdoc.dart';
-import 'package:healthjunction/src/features/authentication/screens/dashboard%20main%20home%20screen/clinichomescreens/clinicuser.dart';
 import 'package:healthjunction/src/features/authentication/screens/hospitalmodulescreens/hospitalscreen1.dart';
 import 'package:healthjunction/src/features/authentication/screens/hospitalmodulescreens/hospitareg.dart';
 import 'package:healthjunction/src/features/authentication/screens/hospitalmodulescreens/phcregisteredhospitals.dart';
--- lib/src/features/authentication/screens/laboratories/laboratories.dart
+++ lib/src/features/authentication/screens/laboratories/laboratories.dart
@@ -7,8 +7,6 @@ import 'package:healthjunction/src/constants/image_string.dart';
 import 'package:healthjunction/src/features/authentication/screens/clinicmodulescreens/clinicscreeen3.dart';
 import 'package:healthjunction/src/features/authentication/screens/clinicmodulescreens/clinicscreen1.dart';
 import 'package:healthjunction/src/features/authentication/screens/clinicmodulescreens/docregistration.dart';
-import 'package:healthjunction/src/features/authentication/screens/dashboard%20main%20home%20screen/clinichomescreens/clinicdoc.dart';
-import 'package:healthjunction/src/features/authentication/screens/dashboard%20main%20home%20screen/clinichomescreens/clinicuser.dart';
 import 'package:healthjunction/src/features/authentication/screens/laboratories/Govtlabs.dart';
 import 'package:healthjunction/src/features/authentication/screens/laboratories/labsmainscreen.dart';
 import 'package:healthjunction/src/features/authentication/screens/profile_icon_functions/profile_page/profile_main_page.dart';
